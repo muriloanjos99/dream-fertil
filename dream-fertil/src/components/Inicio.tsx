f1984e9d162c0ab61a793de2879e66045f0d6140
muriloanjos99 <muriloanjos99@gmail.com>
2025-02-20 19:53:52 -0300

    Feat: Finaliza estrutura da página inicial

--- dream-fertil/src/components/Inicio.tsx
+++ dream-fertil/src/components/Inicio.tsx
@@ -1,23 +1,25 @@
+import { Button } from "@mui/material";
 import "./Inicio.css";
 
 function Inicio() {
   return (
     <section tabIndex={-1} className="inicio-container" data-block-level-container="ClassicSection">
-      <div data-mesh-id="comp-jbnk683finlineContent-gridContainer" data-testid="mesh-container-content">
+      <div className="inicio-content">
         <h2 className="inicio-title">
-          Fertilização orgânica de alta qualidade
+          Fertilização orgânica
+          <span style={{ marginLeft: "0.45em" }}>
+            de {""}
+          </span>
+          alta qualidade
         </h2>
-        <div id="comp-lu052b22" className="HcOXKn c9GqVL QxJLC3 lq2cno YQcXTT comp-lu052b22 wixui-rich-text" data-testid="richTextElement">
-          <p className="font_7 wixui-rich-text__text" style={{ fontSize: '18px' }}>
-            <span className="color_12 wixui-rich-text__text">A Dream Fértil produz fertilizantes 100% orgânicos certificados internacionalmente. Com pesquisa e um moderno processo produtivo, oferecemos uma alternativa sustentável de Alta Produtividade e o melhor Custo Benefício.</span>
-          </p>
+        <p className="inicio-description">
+          A Dream Fértil produz fertilizantes 100% orgânicos certificados internacionalmente. Com pesquisa e um moderno processo produtivo, oferecemos uma alternativa sustentável de Alta Produtividade e o melhor Custo Benefício.
+        </p>
+        <div className="inicio-button-container">
+          <Button className="inicio-button" variant="contained" color="primary" size="large">
+            Entre em contato
+          </Button>
         </div>
-        <div className="comp-jbnk2cj1 FubTgk" id="comp-jbnk2cj1" aria-disabled="false">
-          <a data-testid="linkElement" href="https://wa.me/554499612027" target="_blank" rel="noreferrer noopener" className="uDW_Qe wixui-button PlZyDq" aria-disabled="false">
-            <span className="l7_2fn wixui-button__label">Entre em contato</span>
-          </a>
-        </div>
-        <div data-mesh-id="comp-jbnk683finlineContent-wedge-4"></div>
       </div>
     </section >
   )
