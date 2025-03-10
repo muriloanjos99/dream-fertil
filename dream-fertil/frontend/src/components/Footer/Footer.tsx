3387e6872f86b08f93efd2377c658e25497469ce
muriloanjos99 <muriloanjos99@gmail.com>
2025-03-10 20:21:14 -0300

    Feat: Finaliza footer

--- dream-fertil/frontend/src/components/Footer/Footer.tsx
+++ dream-fertil/frontend/src/components/Footer/Footer.tsx
@@ -1,23 +1,11 @@
+import './Footer.css';
+
 export const Footer = () => {
   return (
-    <footer className="site-footer" id="SITE_FOOTER">
-      <div className="footer-content">
-        <p style={{
-          textAlign: 'center',
-          fontSize: '15px',
-          lineHeight: '1.5em',
-          margin: '1rem 0'
-        }}>
-          <a
-            href="http://www.novai.com.br"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: '#333' }}
-          >
-            © Copyright 2025 - Todos os direitos reservados
-          </a>
-        </p>
-      </div>
-    </footer>
+    <footer>
+      <p className="footer-content">
+        © Copyright 2025 - Todos os direitos reservados
+      </p>
+    </footer >
   );
 };
